105883f450b583154790ef1f51c99aa817eeef03
dgf0000 <79827366+dgf0000@users.noreply.github.com>
2021-06-02 10:12:02 +0900

    Add files via upload

---
+++ My_First_Program_C#_Compiler.bat
@@ -0,0 +1,190 @@
+@echo off 
+color 07 
+mode con cols=50 lines=1 
+title C# Compiler_bw
+::::::::::::::::::::::::::::::::::::::::::::::::::
+::         *컴파일러 수정은 게임 추가 외 금지 *         ::
+::              *비밀번호는 분대장들만*               ::
+::::::::::::::::::::::::::::::::::::::::::::::::::
+
+cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\Hello 
+if exist hello.exe (del hello.exe)
+cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G  
+if exist 1.exe (del 1.exe)
+if exist 2.exe (del 2.exe) 
+if exist 3.exe (del 3.exe) 
+if exist 4.exe (del 4.exe)
+if exist 5.exe (del 5.exe) 
+if exist 6.exe (del 6.exe) 
+if exist 7.exe (del 7.exe) 
+if exist 8.exe (del 8.exe)
+if exist 9.exe (del 9.exe) 
+cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW
+
+:R
+
+%6c%71%8s   %8%9%1%5%6%3%6%2%8%5%6%2%6%1%6     %5%8%9%1%7%7%9%4%3%8%2%9%1
+s%9%4%6e%1%3%5t%1%5 s%8%9m=%4%8#%6%8@%8!%3%1   %9%1%2%3%6%1%5
+s%9%8e%1%5t%1 m%8%9r=%3!%8%4@%9#     %6%5%5   %4%2%3 %9%4%5%6%1%2%3 %5%3 
+%7se%6t b%7w=q%8%9%1%5%3h%5s%7%2%6q%7n%6%1%8%5%4%3%5   %6%1%5%3%4 
+s%8%1%5et /p %6%5b%1%3w%2s%2et=   %6%5p%2as%2sw%2o%5%3rd:%8%7%1%3%9%4%7%2%5%6
+i%7%9f%1%5%3 %bwset%==%sm% %4%5g%8%6o%1%5t%1o%6%8 M%1%5%6%8a%1%5i%5%9n%1%5%5C
+i%8%9%6f %bwset%==%mr% %8%9%4 g%1%6%8%6o%1%3t%8%8%9%6o M%1%6%2%3RC
+i%9%7f %bwset%==%bw% %9%9%1(%8 %7g%9ot%5o L%8S %4%5)%8 %5%2%6el%2s%2e %7(%6%1%2go%2t%1o %9%9L%6F)%7%6%3 
+:LS %5%2%3 %2%3 %1%2%3%5%4%5%6   %5%6%8%9%9%   %2%3%8%9%9%1%5%6 
+%6c%8l%8%3s%4%3%2%8%6%1%5     %6%8%9%1%2%5C%2%6%6%3%6%6      %1%1%2%5%6   %2%3%3%6%8%9%7%5
+c%20%9%1%5%31%8%1%1%7%2%6or %70%1%5a %5%6%1%7%2%3 
+t%8%5i%1%3%5m%4e%8%6o%4u%7%9t%1     /%5t     4%8%9     /%1%5n%1%6o%1%6b%4r%8%7e%9%3a%5k%5                    %5%6
+%5%6%8%4%1%1%3%5 
+g%1o%6t%8%6%1o   %5%3%3%1M%9%6%9a%6i%4%6%2n%9C     %5%5%9%2%3%5%6   %3%5%4%6%8%9
+:LF %8%9%9%2%5%2%3%6%2%6     %8%9%4%5%2%5%5%6%3%6%6%6     %2%2%6%6%3%6%3%3%6     %5%5%6%1%1%2%3 
+cl%9s %8%9%9%1%1%2%3     %2%3%3%6%6%7     %1%2%3%3%3%5%6     %5%5%6%8%5%5%5     %6%6%8%7%2 %4
+t%8%9i%1m%6e%4%8o%7%7%9u%1%3t /%7%9t %1%56 /n%4%2%6%1o%4%8b%6%8r%7%7%9e%1%5a%4%4%3k%1     %6%4%8%4%8%9%1
+g%5o%4t%8%9%5%1%3%6%6%1%3o   %9%9%2%3R%1%8%5%6%1%2%3%8%9%4%5 %2%3 %5%6%4
+
+:Mainc
+color 08
+:Main 
+if exist hello.exe (del hello.exe) 
+set C0a=color0a 
+set C07=color07 
+set C08=color08 
+set gotoMA=MainRoom 
+set gotoMR=MasterRoom
+cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW
+cls 
+mode con cols=106 lines=30 
+echo. 
+echo. 
+echo.
+echo.
+echo.
+echo.
+echo          ***************************************************************************************
+echo               ####       #  #     ####    ####    ## ##   #####  #####  ##    #####   #####
+echo              #    #   ########   #    #  #    #  #  #  #  #    #   #    ##    #       #    #
+echo              #         #  #      #       #    #  #  #  #  #####    #    ##    #####   #####
+echo              #    # ########     #    #  #    #  #  #  #  #        #    ##    #       #    #
+echo               ####   #  #         ####    ####   #  #  #  #      #####  ##### #####   #     #
+echo          ***************************************************************************************
+echo.
+echo                                          -C#Compiler_사용법_소스파일(.cs)생성후_소스파일_이름_입력-
+echo                                                        -C#Compiler이용시_MasterRoom이용_가능-
+echo                                             -MasterRoom-사용법_소스파일(.cs)에_MasterRoom_입력-
+echo.
+echo                  ▽ 〓 ▼ 〓 ▽ 〓 ▼ 〓 ▽ 〓 ▼ 〓 GAME  MODE 〓 ▼ 〓 ▽ 〓 ▼ 〓 ▽ 〓 ▼ 〓 ▽
+echo.
+echo                         테트리스 = 1                 뱀게임 = 2                총알피하기 = 3
+echo.
+echo                         지회찾기 = 4                슈팅게임 = 5                    팩맨 = 6
+echo.
+echo                        플래피버드 = 7                  ??? = 8                    ??? = 9
+echo.
+echo                   △ 〓 ▲ 〓 △ 〓 ▲ 〓 △ 〓 ▲ 〓  †  〓  †  〓 ▲ 〓 △ 〓 ▲ 〓 △ 〓 ▲ 〓 △
+echo.
+echo                                                        Game_Mode_사용법_소스파일에_맞는_숫자_입력
+echo.
+echo                                                                         -ver.3.05.03-
+echo.
+
+set /p name=소스파일(.cs): 
+if %name% == %C0a% (color 0a) 
+if %name% == %C07% (color 07) 
+if %name% == %C08% (color 08) 
+if %name% == %C0a% (goto Main)
+if %name% == %C07% (goto Main) 
+if %name% == %C08% (goto Main) 
+if %name% == %gotoMR% (goto MR)
+set sourcefile=%name%.cs 
+set filename=%name%.exe
+
+if %name% == 1 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 2 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 3 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 4 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 5 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 6 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 7 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 8 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 9 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == hello (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\Hello)
+
+C:\Windows\Microsoft.NET\Framework\%frameworkVer%\csc.exe /out:%filename% /t:exe %sourcefile%
+cls
+echo ---------------------------------------------------------------------------------------------------------
+if exist 1.exe (cls)
+if exist 2.exe (cls)
+if exist 3.exe (cls)
+if exist 4.exe (cls)
+if exist 5.exe (cls)
+if exist 6.exe (cls)
+if exist 7.exe (cls)
+if exist 8.exe (cls)
+if exist 9.exe (cls)
+%filename%
+echo ---------------------------------------------------------------------------------------------------------
+if not %name% == %gotoMR% (PAUSE) 
+echo.
+goto Main
+
+:MRC 
+color 08 
+:MR
+if exist hello.exe (del hello.exe) 
+mode con cols=50 lines=1 
+title Master_Room_BW 
+cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW
+cls
+
+set C0a=color0a 
+set C07=color07
+set C08=color08
+set gotoMA=MainRoom 
+set /p name=
+
+if %name% == %C0a% (color 0a)
+if %name% == %C07% (color 07) 
+if %name% == %C08% (color 08) 
+if %name% == %C0a% (goto MR)
+if %name% == %C07% (goto MR)
+if %name% == %C08% (goto MR) 
+if %name% == %gotoMA% (goto Main)
+
+set sourcefile=%name%.cs 
+set filename=%name%.exe
+
+if %name% == 1 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 2 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 3 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 4 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 5 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+if %name% == 6 (cd S:\Users\aaaa\Desktop\계원백업\당직병\유선\BW\G)
+
+if %name% == notepad (start notepad) 
+if %name% == notepad (goto MR) 
+if %name% == hello.cs (start hello.cs) 
+if %name% == hello.cs (goto MR)
+
+if %name% == hello (mode con cols=80 lines=20) else (mode con cols=106 lines=30)
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:%filename% /t:exe %sourcefile%
+PAUSE 
+echo ---------------------------------------------------------------------------------------------------------
+
+if exist 1.exe (cls)
+if exist 2.exe (cls)
+if exist 3.exe (cls)
+if exist 4.exe (cls)
+if exist 5.exe (cls) 
+if exist 6.exe (cls)
+if exist 7.exe (cls)
+if exist 8.exe (cls) 
+if exist 9.exe (cls)
+
+%filename%
+
+echo ---------------------------------------------------------------------------------------------------------
+
+PAUSE
+goto MR
+
